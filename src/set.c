734c840c9f4d57a4d2b9d2a9b0036e00fd11cdf7
Quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2022-05-30 11:18:31 +0200

    config attribute fixing. Need to fix end of config file.

--- src/set.c
+++ src/set.c
@@ -3,45 +3,54 @@
 #include <string.h>
 #include "../include/set.h"
 
+void get_key(char *str, Set *set)
+{
+    size_t i = 0;
+    unsigned char c;
+    char *key = (char *)malloc(sizeof(char) * strlen(str));
+
+    if (!key)
+        return;
+    while ((c = str[i]) && c != '=' && c != '\n')
+        key[i++] = c;
+    if (*key != 0) key[i++] = 0;
+    set->key = (char *) malloc(sizeof(char) * i);
+    strcpy(set->key, key);
+    free(key);
+}
+
+void get_value(char *str, Set *set)
+{
+    size_t i = 0;
+    size_t j = 0;
+    unsigned char c;
+    unsigned char reading_value = 0;
+    char *value = (char *)malloc(sizeof(char) * strlen(str));
+
+    if (!value)
+        return;
+    while ((c = str[i++]) && !reading_value)
+        if (c == '=')
+            reading_value = 1;
+    if (!reading_value)
+        free(value);
+    i--;
+    while ((c = str[i++]))
+        value[j++] = c;
+    if (*value != 0) value[j++] = 0;
+    set->value = (char *) malloc(sizeof(char) * j);
+    strcpy(set->value, value);
+    free(value);
+}
+
 Set *new_set(char *str)
 {
-    size_t str_length = strlen(str);
     Set *set = (Set *)malloc(sizeof(Set));
-    unsigned char c;
-    unsigned char reading_key = 0;
-    size_t key_length = 0;
-    size_t value_length = 0;
-    size_t l = 0;
+
     if (!set)
         return NULL;
-    for (size_t i = 0; i < str_length; i++)
-    {
-        c = str[i];
-        if (c == '=')
-        {
-            if (!set->key && !set->value)
-            {
-                key_length = sizeof(char) * (i + 1);
-                value_length = sizeof(char) * (str_length - key_length + 1);
-                set->key = (char *)malloc(key_length);
-                set->value = (char *)malloc(value_length);
-                if (!set->key || !set->value)
-                    return NULL;
-                i = -1;
-                reading_key = 1;
-                continue;
-            }
-            reading_key = 0;
-            set->key[l] = 0;
-            l = 0;
-            continue;
-        }
-        if (reading_key && set->key)
-            set->key[l++] = c;
-        if (!reading_key && set->value)
-            set->value[l++] = c;
-    }
-    set->value[l] = 0;
+    get_key(str, set);
+    get_value(str, set);
     return set;
 }
 
